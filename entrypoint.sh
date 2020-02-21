9ba85b2b314f840ba85c9c421375bfd5c5c610ce
Gregory Becker <igreg@users.noreply.github.com>
2020-02-21 17:46:36 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ WEBHOOK_SECRET="$2"
 JOB="$3"
 JOB_TOKEN="$4"
 JOB_PARAMETERS="secret=$WEBHOOK_SECRET&job=$JOB&token=$JOB_TOKEN"
-URL="$HOST/buildByToken/buildWithParameters?BRANCH=$GITHUB_HEAD_REF&$JOB_PARAMETERS"
+URL="$HOST/buildByToken/buildWithParameters?BRANCH=$GITHUB_REF&$JOB_PARAMETERS"
 
 /usr/bin/curl $URL
