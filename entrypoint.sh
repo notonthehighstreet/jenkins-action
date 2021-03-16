50a36fa57e2bd1345481e92c73dbd27da9a15566
Gregory Becker <igreg@users.noreply.github.com>
2021-03-16 19:41:19 +0000

    Fix up

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ HOST="$1"
 WEBHOOK_SECRET="$2"
 JOB_PATH="$3"
 JOB_TOKEN="$4"
-BUILD_PARAMETERS="echo \"$5\" | tr '\n' '&'"
+BUILD_PARAMETERS=$(echo \"$5\" | tr '\n' '&')
 URL="$HOST/buildByToken/buildWithParameters?secret=$WEBHOOK_SECRET&job=$JOB_PATH&token=$JOB_TOKEN&$BUILD_PARAMETERS"
 
 echo /usr/bin/curl $URL
