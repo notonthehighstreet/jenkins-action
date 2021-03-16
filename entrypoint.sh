ad0154730e33a07f5c2746b9a9ba62004d52d40c
Gregory Becker <igreg@users.noreply.github.com>
2021-03-16 20:11:59 +0000

    Escape & and : in job parameter values

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ HOST="$1"
 WEBHOOK_SECRET="$2"
 JOB_PATH="$3"
 JOB_TOKEN="$4"
-JOB_PARAMETERS=$(echo $5 | tr ' ' '&')
+JOB_PARAMETERS=$(echo $5 | sed 's/&/%26/g' | sed 's/:/%3A/g' | tr ' ' '&')
 URL="$HOST/buildByToken/buildWithParameters?secret=$WEBHOOK_SECRET&job=$JOB_PATH&token=$JOB_TOKEN&$JOB_PARAMETERS"
 
 echo /usr/bin/curl $URL
