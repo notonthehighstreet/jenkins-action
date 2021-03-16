afe716c8b617ed1b588451cddc3f8aa61cdcc865
Gregory Becker <igreg@users.noreply.github.com>
2021-03-16 19:59:32 +0000

    Check if JOB_PARAMETERS contains spaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ HOST="$1"
 WEBHOOK_SECRET="$2"
 JOB_PATH="$3"
 JOB_TOKEN="$4"
-JOB_PARAMETERS=$(echo \"$5\" | tr '\n' '&')
+JOB_PARAMETERS=$(echo $5 | tr '\n' '&')
+JOB_PARAMETERS=$(echo $JOB_PARAMETERS | tr ' ' '_')
 URL="$HOST/buildByToken/buildWithParameters?secret=$WEBHOOK_SECRET&job=$JOB_PATH&token=$JOB_TOKEN&$JOB_PARAMETERS"
 
 echo /usr/bin/curl $URL
